180526eb13204d32c55e9e566efec0616ee1a53b
onanypoint <onanypoint@gmail.com>
2018-11-15 10:33:18 +0100

    Update docker files

---
+++ docker-spark-course2.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker run --rm -it -p 8888:8888 -p 50070:50070 -p 8088:8088 -v ${PWD}:/users/jovyan/src bigdatateam/spark-course2
--- docker-yarn-notebooks.sh
+++ docker-yarn-notebooks.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-# Used for
-# - Week 3
-# - Week 6
-
-docker run --rm -it -p 8888:8888 -p 50070:50070 -p 8088:8088 bigdatateam/yarn-notebook
+docker run --rm -it -p 8888:8888 -p 50070:50070 -p 8088:8088 -v ${PWD}:/users/jovyan/src bigdatateam/ bigdatateam/yarn-notebook
